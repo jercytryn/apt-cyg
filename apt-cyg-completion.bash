9a8cd6cc0750a953d3627f22ff3a63d398f2c8c9
Jeremy Cytryn <jercytryn@gmail.com>
2015-06-25 01:41:24 -0400

    initial git completion add

---
+++ apt-cyg-completion.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# bash completion support for apt-cyg
+#
+# Inspired by bash git completion (https://github.com/git/git/blob/master/contrib/completion/git-completion.bash)
+# and bash git flow completion (https://github.com/bobthecow/git-flow-completion/blob/master/git-flow-completion.bash)
+#
+# Installation
+# =====================
+#    1) Download or copy this file somewhere, e.g. ~/.apt-cyg-completion.bash
+#        
+#        curl -L "https://github.com/jercytryn/apt-cyg/raw/master/apt-cyg-completion.bash" -o ~/.apt-cyg-completion.bash
+#
+#    2) Source this file by adding the following line to your .bashrc (adapt to source from wherever you downloaded it):
+#        
+#        source ~/.apt-cyg-completion.bash
+#
+
+
+
